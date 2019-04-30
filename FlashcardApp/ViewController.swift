d518e97b617bdae2986ddbe90332a46560aba732
Steven Xie <sxx107@o804-2.local>
2019-04-29 20:51:43 -0400

    Initial Commit

---
+++ FlashcardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashcardApp
+//
+//  Created by Steven Xie on 4/29/19.
+//  Copyright © 2019 EECS392. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
