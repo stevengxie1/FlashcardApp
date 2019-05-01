aee24985c29b5edb9063681843a41a807a9ac3c8
Kevin Qian <kevinqian@Kevins-MacBook-Pro.local>
2019-05-01 02:37:54 -0400

    pic picker done

--- FlashcardApp/FlashCardViewController.swift
+++ FlashcardApp/FlashCardViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class FlashCardViewController: UIViewController {
+class FlashCardViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var card = FlashCards.FlashCard()
+    var frontImageTapped = false
     
     @IBOutlet weak var cardTitle: UITextField!
     @IBOutlet weak var frontImage: UIImageView!
@@ -25,11 +26,18 @@ class FlashCardViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         cardTitle.text = card.title
-        frontImage.image = card.front.photo
+        if let fimage = card.front.photo{
+            frontImage.image = fimage
+        }
+        if let bimage = card.back.photo{
+            backImage.image = bimage
+        }
         frontText.text = card.front.content
-        backImage.image = card.back.photo
         backText.text = card.back.content
-        
+        frontImage.isMultipleTouchEnabled = true
+        frontImage.isUserInteractionEnabled = true
+        backImage.isMultipleTouchEnabled = true
+        backImage.isUserInteractionEnabled = true
         saveButton.isEnabled = false
         updateSaveButtonState()
     }
@@ -87,5 +95,49 @@ class FlashCardViewController: UIViewController {
         let titleText = cardTitle.text ?? ""
         saveButton.isEnabled = !titleText.isEmpty
     }
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        if frontImageTapped {
+            frontImage.image = selectedImage
+        } else {
+            backImage.image = selectedImage
+        }
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func frontTapped(_ sender: UITapGestureRecognizer) {
+        frontImageTapped = true
+        invokePicSelector()
+    }
+    
+    @IBAction func backTapped(_ sender: Any) {
+        frontImageTapped = false
+        invokePicSelector()
+    }
+    
+    private func invokePicSelector() {
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
 }
